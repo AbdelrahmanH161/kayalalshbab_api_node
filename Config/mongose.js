3371ef997259f956bc153932f2898cb6b9f2d5bb
Abdelrahman Hossam <geo.abdelrahmanem@gmail.com>
2022-12-17 23:59:37 +0200

    mongodb

--- Config/mongose.js
+++ Config/mongose.js
@@ -1,5 +1,15 @@
-const mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://abdelrahman:mVOSes850ZJFl95R@cluster0.kpcj9of.mongodb.net/?retryWrites=true&w=majority').then((x)=>
-{
+// const mongoose = require("mongoose");
+// mongoose.connect('mongodb+srv://abdelrahman:mVOSes850ZJFl95R@cluster0.kpcj9of.mongodb.net/?retryWrites=true&w=majority').then((x)=>
+// {
+//     console.log("connection is open");
+// });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://abdelrahman:mVOSes850ZJFl95R@cluster0.kpcj9of.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
     console.log("connection is open");
+//   const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
