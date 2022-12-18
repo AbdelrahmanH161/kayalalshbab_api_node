64c20c800d2f6ea40f9a6ce2a087da81e8183c40
Abdelrahman Hossam <geo.abdelrahmanem@gmail.com>
2022-12-18 10:11:59 +0200

    /tmp/img

--- Api/Routes/ControlBoard.js
+++ Api/Routes/ControlBoard.js
@@ -8,8 +8,7 @@ import path from 'path';
 ////////////////////multer to uplode image////////////////////////
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // cb(null, "/tmp")
-    cb(null, path.join(process.cwd(), "../../tmp"));
+    cb(null, "/tmp/img")
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now() + file.originalname)
