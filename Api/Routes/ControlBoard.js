34912d58a218bea916248b3a8dc6cf8ec2557406
Abdelrahman Hossam <geo.abdelrahmanem@gmail.com>
2022-12-18 09:17:47 +0200

    path

--- Api/Routes/ControlBoard.js
+++ Api/Routes/ControlBoard.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const multer = require("multer")
 const Item = require("../Models/Item")
 const Category = require("../Models/Category")
+const path = require("path");
 ////////////////////multer to uplode image////////////////////////
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/image")
+    // cb(null, "/image")
+    cb(null, path.join(__dirname, "/image"));
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now() + file.originalname)
